3d491e9a8a6559cec631037aa4ad44d21b9c18bd
Avinash <avinashchowdhury81@gmail.com>
2024-11-09 02:43:54 +0530

    added audio comms

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-import express, { type Request, type Response } from 'express';
+import express, { Request, Response } from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const port = process.env.PORT || 4001;
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,33 +13,28 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", // Adjust to your frontend URL
     methods: ["GET", "POST"],
   },
 });
 
-// Server variables
-let latestCodeVersion: { code: string; uuid?: string } | undefined;
-let countClient = 0;
-
-// API to get the latest code
-app.get('/latest-code', (req: Request, res: Response) => {
-  if (latestCodeVersion) {
-    res.json({ code: latestCodeVersion.code });
-  } else {
-    res.json({ code: 'empty-code' });
-  }
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running.");
 });
 
-// Socket.io events
+let latestCodeVersion: { code: string; uuid?: string } | undefined;
+let activeEditors = 0;
+
+app.get('/latest-code', (req: Request, res: Response) => {
+  res.json({ code: latestCodeVersion ? latestCodeVersion.code : '' });
+});
+
 io.on("connection", (socket: Socket) => {
-  console.log("New client connected");
-  countClient++;
+  activeEditors++;
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    countClient--;
-    if (countClient === 0) {
+    activeEditors--;
+    if (activeEditors === 0) {
       io.emit('code-unlocked', { uuid: 'server' });
     }
   });
@@ -57,6 +51,10 @@ io.on("connection", (socket: Socket) => {
   socket.on('code-unlocked', (msg: { uuid: string }) => {
     io.emit('code-unlocked', msg);
   });
+
+  socket.on('audioStream', (audioData) => {
+    socket.broadcast.emit('audioStream', audioData);
+  });
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Server listening on port ${port}`));
